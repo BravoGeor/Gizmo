b641375640367078beb6c2d24f1328c0fefd78c8
BravoGeor <rlgeorge2004@Gmail.com>
2025-02-05 19:02:24 +0000

    Graphics update
    Graphics design title menu update with Gizmo running, walking audio added

---
+++ Assets/Code/Menus/Settings/MusicSlider.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MusicSlider : MonoBehaviour
+{
+
+    public AudioMixer audioMixer;
+    public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+    }
+
+  public void SetQuality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+}
+
+//Referanced Code https://youtu.be/zc8ac_qUXQY?si=S9OVOwa9kGSQgApV
---
+++ Assets/Code/Menus/Settings/SFslider.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SFSlider: MonoBehaviour
+{
+
+  //  public AudioMixer audioMixer;
+    public void SetSFVolume(float SFvolume)
+    {
+        //   audioMixer.SetFloat("Volume", volume);
+        Debug.Log(SFvolume);
+    }
+
+   // public void SetQuality(int qualityIndex)
+    //{
+      //  QualitySettings.SetQualityLevel(qualityIndex);
+   // }
+}
+
+//Referanced Code https://youtu.be/zc8ac_qUXQY?si=S9OVOwa9kGSQgApV
