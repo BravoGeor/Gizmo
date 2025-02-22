daefed02cae487a3d6f0208b068d57565e566d5c
BravoGeor <rlgeorge2004@Gmail.com>
2025-02-22 22:26:04 +0000

    BASE INTRO, AND BASE CUTSCENE
    Base trees coloured in intro, cutscene space to skip added template

---
+++ Assets/Game/Cutscene/Help.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+public class Help : MonoBehaviour
+{
+    public GameObject myText; // Assign the text to this in the inspector
+
+
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(3);
+        myText.SetActive(true); // Enable the text so it shows
+        yield return new WaitForSeconds(5);
+        myText.SetActive(false); // Disable the text so it is hidden
+    }
+}
